0a72fe1f227a3eb529bcb0f77ad42d8392d9a83f
jojoe2e22 <99125833+jojoe2e22@users.noreply.github.com>
2023-05-16 11:27:56 +0100

    implemented the DVLA API

--- processes/find-vehicle.php
+++ processes/find-vehicle.php
@@ -2,9 +2,39 @@
 
 $registration = $_POST['reg'];
 
-$response->status = 'error';
-$response->message = 'Not yet implemented: '.$registration;
-echo json_encode($response);
-exit;
+
+$url = 'https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles';
+$headers = [
+    'x-api-key: v540xKCqCV4hBAv363yIa5Sd72HPLo223uhu1pkR',
+    'Content-Type: application/json'
+];
+$data = [
+    'registrationNumber' => $registration 
+];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$curl_response = curl_exec($ch);
+
+if ($curl_response === false) {
+    $response->status = 'error';
+    $response->message = 'Error: ' . curl_error($ch);
+    curl_close($ch);
+    echo json_encode($response);
+    exit;
+} else {
+    $curl_data = json_decode($curl_response);
+
+    $response->status = 'success';
+    $response->vehicle_make = $curl_data->make;
+    curl_close($ch);
+    echo json_encode($response);
+    exit;
+}
 
 ?>
